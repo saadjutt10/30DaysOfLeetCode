d9b43735230942f5931ce4a5bf2063168487832f
Muhammad Saad <97842312+saadjutt10@users.noreply.github.com>
2023-10-07 17:02:19 +0500

    Time: 0 ms (100.00%), Space: 42.9 MB (44.75%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -5,6 +5,7 @@ class Solution {
         for(int i=2;i<coordinates.length;i++){
             int x=coordinates[i][0];
             int y= coordinates[i][1];
+            //Using doublePoint formula
             if((coordinates[1][1]-coordinates[0][1])*(x-coordinates[0][0])!= (y-coordinates[0][1])*(coordinates[1][0]-coordinates[0][0])) return false;
         }
         return true;
